6db0fcb403a4dadabe877e4c1a84d84e8893853c
Sergey Yaroslavtsev <sergey.yaroslavtsev@esrf.fr>
2025-08-14 15:58:08 +0200

    full filling the release procedure

--- setup.py
+++ setup.py
@@ -924,7 +924,7 @@ if use_gui():
 setup_requires = ["numpy"]
 
 distrib = setup(name="Pymca_release_test",
-                version='5.9.5.dev11',
+                version=__version__,
                 description=description,
                 author="V. Armando Sole",
                 license="MIT",
--- src/PyMca5/__init__.py
+++ src/PyMca5/__init__.py
@@ -27,7 +27,7 @@ __author__ = "V.A. Sole"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__version__ = "5.9.4"
+__version__ = '5.9.5.dev12'
 
 import os
 import sys
