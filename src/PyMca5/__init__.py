1f463cf96682ebea71f88596a105183fa9ac9db1
Sergey Yaroslavtsev <sergey.yaroslavtsev@esrf.fr>
2026-02-27 14:56:29 +0100

    pre-release dark mod + bug fix

--- src/PyMca5/__init__.py
+++ src/PyMca5/__init__.py
@@ -27,7 +27,7 @@ __author__ = "V.A. Sole"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__version__ = "5.9.6"
+__version__ = "5.9.7rc1"
 
 import os
 import sys
