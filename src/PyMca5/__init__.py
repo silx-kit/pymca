9bfa37acf3ec01a146e82b650998c1fd21d59ae1
Sergey Yaroslavtsev <sergey.yaroslavtsev@esrf.fr>
2026-08-14 11:51:20 +0200

    new RC

--- src/PyMca5/__init__.py
+++ src/PyMca5/__init__.py
@@ -27,7 +27,7 @@ __author__ = "V.A. Sole"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__version__ = "5.9.7rc2"
+__version__ = "5.9.7rc3"
 
 import os
 import sys
